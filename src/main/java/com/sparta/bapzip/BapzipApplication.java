efa81ea4b201f4ca54be2f467085c0614e28bfa7
searchcolor <qkrdydwo1022@gmail.com>
2025-09-26 19:49:07 +0900

    init commit

---
+++ src/main/java/com/sparta/bapzip/BapzipApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.bapzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BapzipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BapzipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/bapzip/BapzipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.bapzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BapzipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
