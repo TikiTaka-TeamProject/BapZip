e929bcc4ccc9fce59673be4bcbb2d59f784ae8a6
Yongeun Kwon <70299766+jenkwon92@users.noreply.github.com>
2025-10-02 16:29:37 +0900

    feat: shop 엔티티 기능 추가

--- src/main/java/com/sparta/bapzip/shop/domain/repository/ShopRepository.java
+++ src/main/java/com/sparta/bapzip/shop/domain/repository/ShopRepository.java
@@ -1,4 +1,11 @@
 package com.sparta.bapzip.shop.domain.repository;
 
-public interface ShopRepository {
+import com.sparta.bapzip.shop.domain.entity.ShopEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ShopRepository extends JpaRepository<ShopEntity, UUID> {
 }
