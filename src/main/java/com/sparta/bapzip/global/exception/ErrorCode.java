a7d1a228193b1c5c77f91637846f85cd94f76e6d
Cro <junsj1230@naver.com>
2025-10-02 10:48:13 +0900

    feat: 전역 에러 핸들러 구현
    ErrorCode: Http 상태값, 에러 메시지 반환 enum 클래스
    ExceptionResponse: 에러 반환 공용 객체
    GlobalException: 상속 받아 구현할 객체
    GlobalizedResponseException: 전역 에러 핸들링

---
+++ src/main/java/com/sparta/bapzip/global/exception/ErrorCode.java
@@ -0,0 +1,13 @@
+package com.sparta.bapzip.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    ;
+    private final HttpStatus status;
+    private final String message;
+}
---
+++ src/main/java/com/sparta/bapzip/global/exception/GlobalException.java
@@ -0,0 +1,13 @@
+package com.sparta.bapzip.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class GlobalException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public GlobalException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+}
