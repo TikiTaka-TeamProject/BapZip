80b7d32f242d278a40c90c3283066a04a922652c
Yongeun Kwon <70299766+jenkwon92@users.noreply.github.com>
2025-10-03 23:58:09 +0900

    feat: Owner 가 이미 가게를 소유하고 있는지 체크하는 로직 추가

--- src/main/java/com/sparta/bapzip/shop/domain/repository/ShopRepository.java
+++ src/main/java/com/sparta/bapzip/shop/domain/repository/ShopRepository.java
@@ -1,10 +1,6 @@
 package com.sparta.bapzip.shop.domain.repository;
 
 import com.sparta.bapzip.shop.domain.entity.ShopEntity;
-import com.sparta.bapzip.user.domain.entity.UserEntity;
-
-import java.util.Optional;
-import java.util.UUID;
 
 public interface ShopRepository{
     ShopEntity save(ShopEntity shop);
