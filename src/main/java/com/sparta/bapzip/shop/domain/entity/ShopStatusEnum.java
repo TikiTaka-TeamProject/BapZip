27b961a6aecd1627af8cbfa9442923b559ee0872
Yongeun Kwon <70299766+jenkwon92@users.noreply.github.com>
2025-10-03 21:28:17 +0900

    refactor: ShopStatus -> ShopStatusEnum 이름 변경

--- src/main/java/com/sparta/bapzip/shop/domain/entity/ShopStatusEnum.java
+++ src/main/java/com/sparta/bapzip/shop/domain/entity/ShopStatusEnum.java
@@ -1,6 +1,6 @@
 package com.sparta.bapzip.shop.domain.entity;
 
-public enum ShopStatus {
+public enum ShopStatusEnum {
     PENDING,    // 등록 후 승인 대기
     APPROVED,   // 매니저 승인 완료
     REJECTED    // 매니저 승인 거부
