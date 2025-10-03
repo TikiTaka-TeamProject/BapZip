2b9ee9f92045be6f59c10afc757ff180624a99aa
Yongeun Kwon <70299766+jenkwon92@users.noreply.github.com>
2025-10-03 23:46:45 +0900

    feat: 가게 생성 에러 처리 로직 추가

--- src/main/java/com/sparta/bapzip/global/exception/ErrorCode.java
+++ src/main/java/com/sparta/bapzip/global/exception/ErrorCode.java
@@ -7,7 +7,15 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    ;
+
+    // Shop 관련 API에서 발생할 수 있는 커스텀 에러
+    // Shop: 공통
+    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "데이터베이스 처리 중 오류가 발생했습니다."),
+    MISSING_REQUIRED_PARAMETER(HttpStatus.BAD_REQUEST, "필수 파라미터가 누락되었습니다."),
+    OWNER_NOT_FOUND(HttpStatus.NOT_FOUND, "가게 소유자를 찾을 수 없습니다."),
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "카테고리를 찾을 수 없습니다."),
+    SERVICE_AREA_NOT_FOUND(HttpStatus.NOT_FOUND, "서비스 지역을 찾을 수 없습니다.");
+
     private final HttpStatus status;
     private final String message;
 }
