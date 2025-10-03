f6e90b6542cfe3dd4ef1b9a8c1d760f86d276860
Yongeun Kwon <70299766+jenkwon92@users.noreply.github.com>
2025-10-03 14:48:18 +0900

    feat: shop 생성 기능 추가

--- src/main/java/com/sparta/bapzip/shop/presentation/dto/response/ResponseDto.java
+++ src/main/java/com/sparta/bapzip/shop/presentation/dto/response/ResponseDto.java
@@ -1,4 +1,17 @@
 package com.sparta.bapzip.shop.presentation.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
 public class ResponseDto {
+    private UUID id;
+    private String name;
+    private String address;
+    private String status;
+    private Double longitude;
+    private Double latitude;
 }
