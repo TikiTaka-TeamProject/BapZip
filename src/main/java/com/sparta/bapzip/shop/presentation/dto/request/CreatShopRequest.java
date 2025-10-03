84783fee8f2dfe9a74a9dbc9c0c5ffcbedc472e9
Yongeun Kwon <70299766+jenkwon92@users.noreply.github.com>
2025-10-03 21:27:23 +0900

    refactor: Shop 생성용 DTO 이름을 용도에 맞게 변경

---
+++ src/main/java/com/sparta/bapzip/shop/presentation/dto/request/CreatShopRequest.java
@@ -0,0 +1,19 @@
+package com.sparta.bapzip.shop.presentation.dto.request;
+
+import com.sparta.bapzip.shop.domain.entity.ShopStatusEnum;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+public class CreatShopRequest {
+    private String name;             // 가게 이름
+    private String address;          // 가게 주소
+    private Double longitude;        // x
+    private Double latitude;         // y
+    private ShopStatusEnum status;
+    private Long ownerId;            // 오너 유저 ID
+    private UUID categoryId;         // 카테고리 ID
+    private UUID serviceAreaId;      // 서비스 지역 ID
+}
+
