14363d1448aa25c65934a0b3d09a9fc016509f80
eNoLJ <derosatam76@gmail.com>
2025-10-07 21:45:28 +0900

    add: import문 추가

--- src/main/java/com/sparta/bapzip/user/domain/entity/UserDetailsImpl.java
+++ src/main/java/com/sparta/bapzip/user/domain/entity/UserDetailsImpl.java
@@ -1,5 +1,6 @@
 package com.sparta.bapzip.user.domain.entity;
 
+import com.sparta.bapzip.user.domain.enums.UserRoleEnum;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
