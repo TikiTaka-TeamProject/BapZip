5bc116f623a6baa5aa753897a2d51a4604999396
eNoLJ <derosatam76@gmail.com>
2025-10-13 14:33:16 +0900

    add: controller 반환 타입 변경

--- src/main/java/com/sparta/bapzip/user/presentation/controller/UserControllerV1.java
+++ src/main/java/com/sparta/bapzip/user/presentation/controller/UserControllerV1.java
@@ -10,6 +10,8 @@ import com.sparta.bapzip.user.domain.entity.UserDetailsImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,41 +23,41 @@ public class UserControllerV1 {
     private final UserServiceV1 userServiceV1;
 
     @PostMapping("/signup")
-    public SignupResponseDto signup(@RequestBody @Valid SignupRequestDto requestDto) {
-        return userServiceV1.signup(requestDto);
+    public ResponseEntity<SignupResponseDto> signup(@RequestBody @Valid SignupRequestDto requestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userServiceV1.signup(requestDto));
     }
 
     @GetMapping
-    public Page<UserResponseDto> getUserList(@RequestParam(value = "page", defaultValue = "1") int page,
-                                             @RequestParam(value = "size", defaultValue = "10") int size,
-                                             @RequestParam(value = "sortBy", defaultValue = "createdAt") String sortBy,
-                                             @RequestParam(value = "isAsc", defaultValue = "false") boolean isAsc) {
+    public ResponseEntity<Page<UserResponseDto>> getUserList(@RequestParam(value = "page", defaultValue = "1") int page,
+                                                             @RequestParam(value = "size", defaultValue = "10") int size,
+                                                             @RequestParam(value = "sortBy", defaultValue = "createdAt") String sortBy,
+                                                             @RequestParam(value = "isAsc", defaultValue = "false") boolean isAsc) {
 
-        return userServiceV1.getUserList(page - 1, size, sortBy, isAsc);
+        return ResponseEntity.ok(userServiceV1.getUserList(page - 1, size, sortBy, isAsc));
     }
 
     @GetMapping("/{userId}")
-    public UserResponseDto getUser(@PathVariable Long userId,
-                                   @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return userServiceV1.getUser(userId, userDetails.getUser());
+    public ResponseEntity<UserResponseDto> getUser(@PathVariable Long userId,
+                                                   @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseEntity.ok(userServiceV1.getUser(userId, userDetails.getUser()));
     }
 
     @PatchMapping
-    public UserUpdateResponseDto updateUser(@RequestBody @Valid UserUpdateRequestDto userUpdateRequestDto,
-                                            @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return userServiceV1.updateUser(userUpdateRequestDto, userDetails.getUser());
+    public ResponseEntity<UserUpdateResponseDto> updateUser(@RequestBody @Valid UserUpdateRequestDto userUpdateRequestDto,
+                                                            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseEntity.ok(userServiceV1.updateUser(userUpdateRequestDto, userDetails.getUser()));
     }
 
     @DeleteMapping
-    public UserDeleteResponseDto deleteUser(@RequestBody @Valid UserDeleteRequestDto userDeleteRequestDto,
-                                            @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return userServiceV1.deleteUser(userDeleteRequestDto, userDetails.getUser());
+    public ResponseEntity<UserDeleteResponseDto> deleteUser(@RequestBody @Valid UserDeleteRequestDto userDeleteRequestDto,
+                                                            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseEntity.ok(userServiceV1.deleteUser(userDeleteRequestDto, userDetails.getUser()));
     }
 
     @PatchMapping("/{userId}/role")
-    public UserRoleChangeResponseDto changeUserRole(@PathVariable Long userId,
-                                                    @RequestBody @Valid UserRoleChangeRequestDto userRoleChangeRequestDto,
-                                                    @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return userServiceV1.changeUserRole(userId, userRoleChangeRequestDto, userDetails.getUser());
+    public ResponseEntity<UserRoleChangeResponseDto> changeUserRole(@PathVariable Long userId,
+                                                                    @RequestBody @Valid UserRoleChangeRequestDto userRoleChangeRequestDto,
+                                                                    @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseEntity.ok(userServiceV1.changeUserRole(userId, userRoleChangeRequestDto, userDetails.getUser()));
     }
 }
