620b3155b3c1b52612b8b60b0bbddb95556110a5
searchcolor <qkrdydwo1022@gmail.com>
2025-10-13 19:24:44 +0900

    fix: ApiResponse 적용

--- src/main/java/com/sparta/bapzip/servicearea/presentation/controller/ServiceAreaControllerV1.java
+++ src/main/java/com/sparta/bapzip/servicearea/presentation/controller/ServiceAreaControllerV1.java
@@ -1,12 +1,12 @@
 package com.sparta.bapzip.servicearea.presentation.controller;
 
+import com.sparta.bapzip.global.response.ApiResponse;
 import com.sparta.bapzip.servicearea.application.ServiceAreaServiceV1;
 import com.sparta.bapzip.servicearea.application.dto.AreaSaveDto;
 import com.sparta.bapzip.servicearea.application.dto.request.AreaSaveRequest;
 import com.sparta.bapzip.servicearea.presentation.dto.response.AreaSaveResponse;
 import com.sparta.bapzip.user.domain.entity.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,15 +23,13 @@ public class ServiceAreaControllerV1 {
 
 
     @PostMapping
-    public ResponseEntity<AreaSaveResponse> saveServiceArea(
+    public ResponseEntity<ApiResponse<AreaSaveResponse>> saveServiceArea(
             @RequestBody AreaSaveRequest request,
             @AuthenticationPrincipal UserDetailsImpl userDetails
             ){
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(AreaSaveResponse.from(
-                        service.createServiceArea(AreaSaveDto.from(request))
-                ));
+        return ApiResponse.created(
+                AreaSaveResponse.from(
+                        service.createServiceArea(AreaSaveDto.from(request))));
     }
 
 
