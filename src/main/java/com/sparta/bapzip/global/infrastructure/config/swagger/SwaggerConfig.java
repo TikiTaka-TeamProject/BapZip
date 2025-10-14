e81a3fbbfacb121512473c9cf4bdbe463c5b4d3d
searchcolor <qkrdydwo1022@gmail.com>
2025-10-15 01:58:51 +0900

    fix: Swagger config 설정

--- src/main/java/com/sparta/bapzip/global/infrastructure/config/swagger/SwaggerConfig.java
+++ src/main/java/com/sparta/bapzip/global/infrastructure/config/swagger/SwaggerConfig.java
@@ -1,26 +1,115 @@
 package com.sparta.bapzip.global.infrastructure.config.swagger;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "TikiTaka-TeamProject BapZip Api Documents",
+                description = """
+                        BapZip API Description
+                        """,
+                version = "v1.0.0",
+                contact = @Contact(name = "TikiTaka-TeamProject", url = "https://github.com/TikiTaka-TeamProject")
+                )
+        )
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Collections.singletonList(securityRequirement));
     }
 
-    private Info apiInfo() {
-        return new Info()
-                .title("BapZip")
-                .description("Api Documents")
-                .version("1.0.0");
+    @Bean
+    public GroupedOpenApi userApi() {
+        return GroupedOpenApi.builder()
+                .group("user")
+                .pathsToMatch("/v1/users/**")
+                .build();
     }
+
+    @Bean
+    public GroupedOpenApi shopApi() {
+        return GroupedOpenApi.builder()
+                .group("shop")
+                .pathsToMatch("/v1/shops/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi aiApi() {
+        return GroupedOpenApi.builder()
+                .group("ai-log")
+                .pathsToMatch("/v1/ai-logs/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi categoryApi() {
+        return GroupedOpenApi.builder()
+                .group("category")
+                .pathsToMatch("/v1/categories/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi menuApi() {
+        return GroupedOpenApi.builder()
+                .group("menu")
+                .pathsToMatch("/v1/menus/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi orderApi() {
+        return GroupedOpenApi.builder()
+                .group("order")
+                .pathsToMatch("/v1/orders/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi paymentApi() {
+        return GroupedOpenApi.builder()
+                .group("payment")
+                .pathsToMatch("/v1/payments/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi reviewApi() {
+        return GroupedOpenApi.builder()
+                .group("review")
+                .pathsToMatch("/v1/reviews/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi serviceAreaApi() {
+        return GroupedOpenApi.builder()
+                .group("service-area")
+                .pathsToMatch("/v1/service-areas/**")
+                .build();
+    }
+
 }
