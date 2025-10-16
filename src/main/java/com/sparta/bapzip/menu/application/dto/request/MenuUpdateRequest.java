0811f3945db242ab296f98f550d502c81356c7ac
Sona <objet_an@naver.com>
2025-10-16 12:47:23 +0900

    add: 메뉴 API controller / dto Swagger 자동화 어노테이션 추가

--- src/main/java/com/sparta/bapzip/menu/application/dto/request/MenuUpdateRequest.java
+++ src/main/java/com/sparta/bapzip/menu/application/dto/request/MenuUpdateRequest.java
@@ -1,16 +1,21 @@
 package com.sparta.bapzip.menu.application.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "메뉴 수정 요청 DTO")
 public record MenuUpdateRequest(
+        @Schema(description = "메뉴 이름", example = "매운 불고기 덮밥")
         String name,
 
+        @Schema(description = "메뉴 설명", example = "매콤하게 조리된 불고기 덮밥")
         @Size(max = 255, message = "내용은 최대 255자 이내로 작성해야 합니다.")
         String content,
 
         // update Integer => null값 허용 (PATCH)
+        @Schema(description = "메뉴 가격", example = "9000")
         @Min(value = 0, message = "가격은 0원 이상이어야 합니다.")
         @Max(value = 9999999, message = "메뉴 가격의 허용 범위를 초과했습니다.")
         Integer price
--- src/main/java/com/sparta/bapzip/menu/presentation/dto/response/MenuCreateResponse.java
+++ src/main/java/com/sparta/bapzip/menu/presentation/dto/response/MenuCreateResponse.java
@@ -2,15 +2,29 @@ package com.sparta.bapzip.menu.presentation.dto.response;
 
 import com.sparta.bapzip.menu.domain.entity.MenuEntity;
 import com.sparta.bapzip.menu.domain.enums.MenuStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.UUID;
 
+@Schema(description = "메뉴 생성 응답 DTO")
 public record MenuCreateResponse(
+
+        @Schema(description = "메뉴 UUID", example = "550e8400-e29b-41d4-a716-446655440000")
         UUID id,
+
+        @Schema(description = "메뉴 이름", example = "불고기 덮밥")
         String name,
+
+        @Schema(description = "메뉴 설명", example = "달콤한 불고기와 밥이 어우러진 메뉴")
         String content,
+
+        @Schema(description = "메뉴 가격", example = "8500")
         int price,
+
+        @Schema(description = "메뉴 상태", example = "AVAILABLE")
         MenuStatus status,
+
+        @Schema(description = "가게 UUID", example = "550e8400-e29b-41d4-a716-446655440000")
         UUID shopId
 ) {
     public static MenuCreateResponse from(MenuEntity menu) {
