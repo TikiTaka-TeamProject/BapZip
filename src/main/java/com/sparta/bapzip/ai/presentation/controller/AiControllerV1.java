5c6680b1c6ccc19490ad476dc614c1c9a9c2385d
searchcolor <qkrdydwo1022@gmail.com>
2025-10-17 10:39:23 +0900

    docs: Swagger annotation 작성 및 pageable 수정

--- src/main/java/com/sparta/bapzip/ai/presentation/controller/AiControllerV1.java
+++ src/main/java/com/sparta/bapzip/ai/presentation/controller/AiControllerV1.java
@@ -3,7 +3,10 @@ package com.sparta.bapzip.ai.presentation.controller;
 import com.sparta.bapzip.ai.application.AiServiceV1;
 import com.sparta.bapzip.ai.application.dto.AiLogResponseDto;
 import com.sparta.bapzip.global.response.ApiResponse;
+import com.sparta.bapzip.global.response.PageResponseDto;
 import com.sparta.bapzip.user.domain.entity.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,22 +19,27 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/v1/ai-logs")
+@Tag(name = "Ai-log", description = "Ai-log api")
 public class AiControllerV1 {
 
     private final AiServiceV1 aiServiceV1;
 
-
+    @Operation(summary = "Ai-log 단건 조회", description = "Ai-log 단건조회 메서드 입니다.")
     @GetMapping("/{aiLogId}")
     public ResponseEntity<ApiResponse<AiLogResponseDto>> getAiLog(@PathVariable UUID aiLogId){
         AiLogResponseDto aiLogResponseDto = aiServiceV1.getAiLog(aiLogId);
         return ApiResponse.ok(aiLogResponseDto);
     }
 
+    @Operation(summary = "Ai-log 다건 조회", description = "Ai-log 다건조회 메서드 입니다.")
     @GetMapping()
-    public ResponseEntity<ApiResponse<Page<AiLogResponseDto>>> getAiLogs(
+    public ResponseEntity<ApiResponse<PageResponseDto<AiLogResponseDto>>> getAiLogs(
             Pageable pageable,
+            @RequestParam(value = "isAsc", defaultValue = "false") boolean isAsc,
             @AuthenticationPrincipal UserDetailsImpl userDetails){
-        return ApiResponse.ok(aiServiceV1.getAiLogs(userDetails.getUser(), pageable));
+
+        Page<AiLogResponseDto> page = aiServiceV1.getAiLogs(userDetails.getUser(), pageable);
+        return ApiResponse.ok(PageResponseDto.fromPage(page,pageable.getSort().toString(),isAsc));
     }
 
     @PostMapping
